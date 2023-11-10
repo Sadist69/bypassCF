5bd5b487eb07ba9f87ea65020a5d73c2d966940d
SADIST <148618569+Sadist69@users.noreply.github.com>
2023-11-10 20:18:26 +0530

    install.py

---
+++ install.py
@@ -0,0 +1,15 @@
+import os ,system
+os.system("sudo apt update")
+os.system("sudo apt install nodejs")
+os.system("sudo npm install nodejs")
+os.system("sudo npm install http2")
+os.system("sudo npm install url")
+os.system("sudo npm install user-agents")
+os.system("sudo npm install fake-useragents")
+os.system("sudo npm install colors")
+os.system("sudo npm install fs")
+os.system("sudo npm install crypto")
+os.system("sudo npm install cluster")
+os.system("sudo npm install tls")
+os.system("sudo npm install net")
+
